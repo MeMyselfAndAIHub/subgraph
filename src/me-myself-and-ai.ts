4a236fb03f0e94edb3d6eddf8f29a54c328d5f8f
franfran20 <rickyegboluche@gmail.com>
2023-06-11 04:31:16 -0700

    first commit

--- src/me-myself-and-ai.ts
+++ src/me-myself-and-ai.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import {
   MeMyselfAndAi,
   Medication_Created,
@@ -8,85 +8,230 @@ import {
   Medication_Deleted,
   Daily_Routine_Deleted,
   Appointment_Deleted,
-  Daily_Reminder_Pushed
-} from "../generated/MeMyselfAndAi/MeMyselfAndAi"
-import { ExampleEntity } from "../generated/schema"
+  Daily_Reminder_Pushed,
+} from "../generated/MeMyselfAndAi/MeMyselfAndAi";
+import {
+  Medication,
+  DailyReminder,
+  DailyRoutine,
+  Appointment,
+  Tag,
+} from "../generated/schema";
 
 export function handleMedication_Created(event: Medication_Created): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+  let medication = Medication.load(
+    generateReminderId(
+      event.params.user,
+      event.params.reminder_id,
+      "Medication"
+    )
+  );
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
+  if (!medication)
+    medication = new Medication(
+      generateReminderId(
+        event.params.user,
+        event.params.reminder_id,
+        "Medication"
+      )
+    );
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  medication.reminderId = event.params.reminder_id;
+  medication.userAddress = event.params.user;
+  medication.totalTabsAmount = event.params.med.total_tabs_amount;
+  medication.days = event.params.med.days;
+  medication.name = event.params.med.name;
+  medication.dosage = event.params.med.dosage;
+  medication.desc = event.params.med.desc;
+  medication.deleted = false;
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.user = event.params.user
-  entity.med_total_tabs_amount = event.params.med.total_tabs_amount
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.can_decrypt(...)
-  // - contract.get_account_details(...)
-  // - contract.get_next_iris_id(...)
-  // - contract.get_user_w3memoryname_and_signing_key(...)
-  // - contract.deployer(...)
-  // - contract.iris_identifier_count(...)
-  // - contract.accounts(...)
-  // - contract.has_account(...)
-  // - contract.taken_username(...)
-  // - contract.reminder_id(...)
-  // - contract.users_meds(...)
-  // - contract.users_daily_routine(...)
-  // - contract.users_appointments(...)
-  // - contract.tags(...)
-  // - contract.tag_id(...)
-  // - contract.iris_id_to_memory(...)
-  // - contract.all_users(...)
-  // - contract.validity(...)
+  medication.save();
 }
 
 export function handleDaily_Routine_Created(
   event: Daily_Routine_Created
-): void {}
+): void {
+  let dailyRoutine = DailyRoutine.load(
+    generateReminderId(
+      event.params.user,
+      event.params.reminder_id,
+      "DailyRoutine"
+    )
+  );
 
-export function handleAppointment_Created(event: Appointment_Created): void {}
+  if (!dailyRoutine)
+    dailyRoutine = new DailyRoutine(
+      generateReminderId(
+        event.params.user,
+        event.params.reminder_id,
+        "DailyRoutine"
+      )
+    );
+  dailyRoutine.reminderId = event.params.reminder_id;
+  dailyRoutine.userAddress = event.params.user;
+  dailyRoutine.days = event.params.daily_routine.days;
+  dailyRoutine.importance = event.params.daily_routine.importance;
+  dailyRoutine.desc = event.params.daily_routine.routine_description;
+  dailyRoutine.deleted = false;
 
-export function handleTag_Created(event: Tag_Created): void {}
+  dailyRoutine.save();
+}
 
-export function handleMedication_Deleted(event: Medication_Deleted): void {}
+export function handleAppointment_Created(event: Appointment_Created): void {
+  let appointment = Appointment.load(
+    generateReminderId(
+      event.params.user,
+      event.params.reminder_id,
+      "Appointment"
+    )
+  );
+
+  if (!appointment)
+    appointment = new Appointment(
+      generateReminderId(
+        event.params.user,
+        event.params.reminder_id,
+        "Appointment"
+      )
+    );
+
+  appointment.reminderId = event.params.reminder_id;
+  appointment.userAddress = event.params.user;
+  appointment.days = event.params.appoitment.days;
+  appointment.importance = event.params.appoitment.importance;
+  appointment.desc = event.params.appoitment.appointment_desc;
+  appointment.locationDescription = event.params.appoitment.location;
+  appointment.deleted = false;
+
+  appointment.save();
+}
+
+export function handleTag_Created(event: Tag_Created): void {
+  let tag = Tag.load(
+    generateTagId(
+      event.params.user,
+      event.params.tag.name,
+      event.params.tag.summary,
+      event.params.tag.importance.toString()
+    )
+  );
+
+  if (!tag)
+    tag = new Tag(
+      generateTagId(
+        event.params.user,
+        event.params.tag.name,
+        event.params.tag.summary,
+        event.params.tag.importance.toString()
+      )
+    );
+
+  tag.userAddress = event.params.user;
+  tag.name = event.params.tag.name;
+  tag.importance = event.params.tag.importance;
+  tag.summary = event.params.tag.summary;
+  tag.importance = event.params.tag.importance;
+
+  tag.save();
+}
+
+export function handleMedication_Deleted(event: Medication_Deleted): void {
+  let medication = Medication.load(
+    generateReminderId(
+      event.params.user,
+      event.params.reminder_id,
+      "Medication"
+    )
+  );
+
+  if (medication) {
+    medication.deleted = true;
+    medication.save();
+  }
+}
 
 export function handleDaily_Routine_Deleted(
   event: Daily_Routine_Deleted
-): void {}
+): void {
+  let dailyRoutine = DailyRoutine.load(
+    generateReminderId(
+      event.params.user,
+      event.params.reminder_id,
+      "DailyRoutine"
+    )
+  );
 
-export function handleAppointment_Deleted(event: Appointment_Deleted): void {}
+  if (dailyRoutine) {
+    dailyRoutine.deleted = true;
+
+    dailyRoutine.save();
+  }
+}
+
+export function handleAppointment_Deleted(event: Appointment_Deleted): void {
+  let appointment = Appointment.load(
+    generateReminderId(
+      event.params.user,
+      event.params.reminder_id,
+      "Appointment"
+    )
+  );
+
+  if (appointment) {
+    appointment.deleted = true;
+
+    appointment.save();
+  }
+}
 
 export function handleDaily_Reminder_Pushed(
   event: Daily_Reminder_Pushed
-): void {}
+): void {
+  let dailyReminder = DailyReminder.load(
+    generateReminderPushedId(
+      event.params.user,
+      event.params.username,
+      event.params.time
+    )
+  );
+
+  if (!dailyReminder)
+    dailyReminder = new DailyReminder(
+      generateReminderPushedId(
+        event.params.user,
+        event.params.username,
+        event.params.time
+      )
+    );
+
+  dailyReminder.userAddress = event.params.user;
+  dailyReminder.userName = event.params.username;
+  dailyReminder.time = event.params.time;
+
+  dailyReminder.save();
+}
+
+function generateReminderId(
+  user: Bytes,
+  reminderId: BigInt,
+  reminderType: String
+): string {
+  return reminderId.toHexString() + user.toHexString() + reminderType;
+}
+
+function generateTagId(
+  user: Bytes,
+  name: String,
+  summary: String,
+  importance: String
+): string {
+  return user.toHexString() + name + summary + importance;
+}
+
+function generateReminderPushedId(
+  user: Bytes,
+  username: String,
+  time: BigInt
+): string {
+  return user.toHexString() + username + time.toString();
+}
